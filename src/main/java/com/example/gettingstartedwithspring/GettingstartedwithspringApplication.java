bbd2523a0a3744df7a8e9916539ca38b5959858a
Jayson Soria <jaysonsoria517@gmail.com>
2023-09-05 08:51:25 +0800

    First Commit

---
+++ src/main/java/com/example/gettingstartedwithspring/GettingstartedwithspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.gettingstartedwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingstartedwithspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingstartedwithspringApplication.class, args);
+	}
+
+}
